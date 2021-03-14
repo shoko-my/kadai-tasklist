66bf0dc8d6775376ff54b62f33eba9814055d37c
shoko <shoko.my0@gmail.com>
2021-03-15 01:31:54 +0900

    Add validation

---
+++ src/models/validators/TaskValidator.java
@@ -0,0 +1,25 @@
+package models.validators;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import models.Task;
+
+public class TaskValidator {
+    public static List<String> validate(Task t){
+        List<String>errors = new ArrayList<String>();
+
+    String content_error = validateContent(t.getContent());
+    if(!content_error.equals("")){
+        errors.add(content_error);
+    }
+    return errors;
+    }
+
+public static String validateContent(String content){
+    if(content == null || content.equals("")){
+        return"メッセージを入力してください。";
+    }
+    return"";
+  }
+}
